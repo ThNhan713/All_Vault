a646dc3c8882dd349adf755092ded89b24d8b457
Nhan <nhando7132@gmail.com>
2026-09-04 13:24:28 +0700

    setup convex get and insert documents

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import { SignInButton, UserButton } from "@clerk/nextjs";
-import { Authenticated, AuthLoading, AuthRefreshing, Unauthenticated } from "convex/react";
-import { Content } from "next/font/google";
-import Image from "next/image";
+import { Authenticated, AuthLoading, AuthRefreshing, Unauthenticated, useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 export default function Home() {
+  const documents = useQuery(api.documents.getDocuments)
+  const createDocument = useMutation(api.documents.createDocument);
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -14,7 +16,8 @@ export default function Home() {
         </Unauthenticated>
         <Authenticated>
           <UserButton />
-
+          <button onClick={() => { createDocument({ title: "test doc" }) }}>Test create docs</button>
+          {documents?.map((document) => <div key={document._id}>{document.title}</div>)}
         </Authenticated>
         <AuthLoading>
           <p>Still loading</p>
